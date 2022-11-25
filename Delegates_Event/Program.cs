dbfee8a7d98af979cd2cfc33a2da3537eb7df63b
Faruk ul Islam <faruk.ice.pust@gmail.com>
2022-11-26 00:01:41 +0600

    first_add

---
+++ Delegates_Event/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delegates_Event
+{
+    public delegate int delegatefun(int x ,int y);
+     class Program
+    {public static int addnumber(int f1,int f2)
+        {
+          return f1 + f2;
+            
+        }
+        static void Main(string[] args)
+        {
+           // delegatefun x =new delegatefun(a)
+           // int delega(int x, int y) { }
+        }
+    }
+}
